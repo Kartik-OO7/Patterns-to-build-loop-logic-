c16fa2f6e46faa76d00d0bcc627bb3973e82de5e
Kartik-OO7 <kartik.kumar.ashok@gmail.com>
2026-07-01 00:44:14 +0530

    Added first pattern of square

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 
 In this repository,
 I am solving 22 problems based on pattern, which would help in further DSA.
+I am using input output format for the output results.
--- pattern01.cpp
+++ pattern01.cpp
@@ -1,8 +1,18 @@
 #include<iostream>
 using namespace std;
 
-
+//Porgram to print a square 
+void square(int n){
+    for(int i=0;i<n;i++){
+        for(int j=0 ; j<n;j++){
+            cout<< "* ";
+        }
+        cout <<endl;
+    }
+}
 int main() {
-    
+    int n;
+    cin >> n;
+    square(n);
     return 0;
 }
