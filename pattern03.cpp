53e9137f80e08c9a61085c2f464fcc74840694e4
Kartik-OO7 <kartik.kumar.ashok@gmail.com>
2026-07-01 09:53:51 +0530

    Feat: Added pattern 03

---
+++ pattern03.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+//printing 3rd pattern of right angled triangle with numbers 
+void pattern3(int n){
+    for(int i=1; i<=n;i++){
+        for(int j=1 ;j<=i; j++){
+            cout << j << " ";
+        }
+        cout << endl;
+    }
+}
+
+int main() {
+    int n;
+    cin >> n;
+    pattern3(n);
+    return 0;
+}
